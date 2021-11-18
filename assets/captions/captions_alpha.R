d71a24735021c357a8437dce2c15aaad3b0b07ce
jarrodscott <jarrod.jude.scott@gmail.com>
2021-11-18 18:47:31 -0500

    added taxa & alpha wf

--- assets/captions/captions_alpha.R
+++ assets/captions/captions_alpha.R
@@ -3,15 +3,23 @@
 caption_tab_ssu("ssu_alpha_div", "Hill numbers for samples in the unfiltered & filtered data sets.")
 caption_tab_ssu("ssu_norm_tests", "Summary of Normality Tests for unfiltered & filtered data sets. P-values in red indicate significance (p-value < 0.05).")
 caption_tab_ssu("ssu_sign_tests", "Summary of significance tests for unfiltered & filtered data sets. P-values in red for post hoc analysis indicate significance (p-value < 0.05).")
+caption_tab_ssu("ssu_alpha_hill_posthoc", "Summary of posthoc comparisons for unfiltered & filtered data sets across three metrics. P-values in red indicate significance between year/treatment (p-value < 0.05).")
 
 caption_tab_its("its_alpha_div", "Hill numbers for samples in the unfiltered & filtered data sets.")
 caption_tab_its("its_norm_tests", "Summary of Normality Tests for unfiltered & filtered data sets. P-values in red indicate significance (p-value < 0.05).")
 caption_tab_its("its_sign_tests", "Summary of significance tests for unfiltered & filtered data sets. P-values in red for post hoc analysis indicate significance (p-value < 0.05).")
+caption_tab_its("its_alpha_hill_posthoc", "Summary of posthoc comparisons for unfiltered & filtered data sets across three metrics. P-values in red indicate significance between year/treatment (p-value < 0.05).")
 #<small>`r caption_tab_ssu("qual_scores_after_ssu")`</small>
 
 ## Figure captions
-caption_fig_ssu("ssu_alpha_div_plots", "Alpha diversity plots. Top row = Observed; middle row = Shannon exponential; bottom row = Inverse Simpson. From left to right, Full (unfiltered) data, Arbitrary filtered, PERfect filtered, PIME filtered.")
+caption_fig_ssu("ssu_alpha_div_plots_full", "Alpha diversity plots. Left = Observed richness; middle = Shannon exponential; right = Simpson multiplicative inverse.")
+caption_fig_ssu("ssu_alpha_div_plots_filt", "Alpha diversity plots. Left = Observed richness; middle = Shannon exponential; right = Simpson multiplicative inverse.")
+caption_fig_ssu("ssu_alpha_div_plots_perfect", "Alpha diversity plots. Left = Observed richness; middle = Shannon exponential; right = Simpson multiplicative inverse.")
+caption_fig_ssu("ssu_alpha_div_plots_pime", "Alpha diversity plots. Left = Observed richness; middle = Shannon exponential; right = Simpson multiplicative inverse.")
 
-caption_fig_its("its_alpha_div_plots", "Alpha diversity plots. Top row = Observed; middle row = Shannon exponential; bottom row = Inverse Simpson. From left to right, Full (unfiltered) data, Arbitrary filtered, PERfect filtered, PIME filtered.")
+caption_fig_its("its_alpha_div_plots_full", "Alpha diversity plots. Left = Observed richness; middle = Shannon exponential; right = Simpson multiplicative inverse.")
+caption_fig_its("its_alpha_div_plots_filt", "Alpha diversity plots. Left = Observed richness; middle = Shannon exponential; right = Simpson multiplicative inverse.")
+caption_fig_its("its_alpha_div_plots_perfect", "Alpha diversity plots. Left = Observed richness; middle = Shannon exponential; right = Simpson multiplicative inverse.")
+caption_fig_its("its_alpha_div_plots_pime", "Alpha diversity plots. Left = Observed richness; middle = Shannon exponential; right = Simpson multiplicative inverse.")
 #<small>`r caption_fig_ssu("ssu_alpha_div_plots")`</small>
 
