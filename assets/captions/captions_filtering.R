17d4da14bbf9b09db4684a659aa51ff1cd87f7be
jarrodscott <jarrod.jude.scott@gmail.com>
2021-12-02 18:50:51 -0500

    changed to year split filter

--- assets/captions/captions_filtering.R
+++ assets/captions/captions_filtering.R
@@ -1,39 +1,39 @@
 ## Table captions
 ## Summary
-caption_tab_ssu("ssu_all_filt_summary", "Summary of Arbitrary, PERfect, and PIME filtering.")
-caption_tab_ssu("ssu_all_filt_samp_summary", "Sample summary showing the number of reads and ASVs after Arbitrary, PERfect, and PIME filtering.")
+caption_tab_ssu("ssu_filtering_results", "Summary of Arbitrary, PERfect, and PIME filtering.")
+caption_tab_ssu("ssu_filter_sample_data", "Sample summary showing the number of reads and ASVs after Arbitrary, PERfect, and PIME filtering.")
 
-caption_tab_its("its_all_filt_summary", "Summary of Arbitrary, PERfect, and PIME filtering.")
-caption_tab_its("its_all_filt_samp_summary", "Sample summary showing the number of reads and ASVs after Arbitrary, PERfect, and PIME filtering.")
+caption_tab_its("its_filtering_results", "Summary of Arbitrary, PERfect, and PIME filtering.")
+caption_tab_its("its_filter_sample_data", "Sample summary showing the number of reads and ASVs after Arbitrary, PERfect, and PIME filtering.")
 
 ## Arbitrary
 caption_tab_ssu("ssu_arbitrary_filt_summary", "Summary of arbitrary filtering where ASVs represented by fewer than 5 reads, present in less than 20% of samples, and/or a variance less than 0.2, were removed.")
-caption_tab_ssu("ssu_filt_samp_summary", "Sample summary showing the number of reads and ASVs after Arbitrary filtering.")
+caption_tab_ssu("ssu_arbitrary_filt_samples", "Sample summary showing the number of reads and ASVs after Arbitrary filtering.")
 
 caption_tab_its("its_arbitrary_filt_summary", "Summary of arbitrary filtering where ASVs represented by fewer than 5 reads, present in less than 20% of samples, and/or a variance less than 0.2, were removed.")
-caption_tab_its("its_filt_samp_summary", "Sample summary showing the number of reads and ASVs after Arbitrary filtering.")
+caption_tab_its("its_arbitrary_filt_samples", "Sample summary showing the number of reads and ASVs after Arbitrary filtering.")
 
 ## PERfect
 caption_tab_ssu("ssu_perfect_filt_summary", "Summary of PERfect filtering using the decreased order and filtering p-values < 0.05.")
 caption_tab_its("its_perfect_filt_summary", "Summary of PERfect filtering using the defualt order and filtering p-values < 0.05.")
-caption_tab_ssu("ssu_perfect_samp_summary", "Sample summary showing the number of reads and ASVs after PERfect filtering.")
-caption_tab_its("its_perfect_samp_summary", "Sample summary showing the number of reads and ASVs after PERfect filtering.")
+caption_tab_ssu("ssu_perfect_filt_samples", "Sample summary showing the number of reads and ASVs after PERfect filtering.")
+caption_tab_its("its_perfect_filt_samples", "Sample summary showing the number of reads and ASVs after PERfect filtering.")
 
 ## PIME
 caption_tab_ssu("ssu_pime_filt_top_asvs", "Summary of top 30 ASVs from the chosen prevalence interval.")
 caption_tab_ssu("ssu_pime_est_error", "Results of 100 random permutations for each prevalence interval based on a function that randomizes the samples labels into arbitrary groupings. using n random permutations. For each randomized and prevalence filtered data set, the OOB error rate is calculated to estimate whether the original differences in groups of samples occur by chance. ")
-caption_tab_ssu("ssu_pime_samp_summary", "Sample summary showing the number of reads and ASVs after PIME filtering.")
+caption_tab_ssu("ssu_pime_filt_samples", "Sample summary showing the number of reads and ASVs after PIME filtering.")
 caption_tab_ssu("ssu_pime_oob_best_prev", "Out of Bag error (OOB) from random forests for each prevalence interval. The number of taxa and the number of remaining reads for each prevalence interval are shown.")
 caption_tab_ssu("ssu_pime_filt_summary", "Comparison of the unfiltered data and PIME filtered data for the chosen prevalence interval.")
-caption_tab_ssu("ssu_asv_pime_sum", "Changes in read count and total ASVs during PIME filtering.")
+caption_tab_ssu("ssu_pime_change_by_year_treat", "Changes in read count and total ASVs during PIME filtering.")
 
 
 caption_tab_its("its_pime_filt_top_asvs", "Summary of top 30 ASVs from the chosen prevalence interval.")
 caption_tab_its("its_pime_est_error", "Results of 100 random permutations for each prevalence interval based on a function that randomizes the samples labels into arbitrary groupings. using n random permutations. For each randomized and prevalence filtered data set, the OOB error rate is calculated to estimate whether the original differences in groups of samples occur by chance. ")
-caption_tab_its("its_pime_samp_summary", "Sample summary showing the number of reads and ASVs after PIME filtering.")
+caption_tab_its("its_pime_filt_samples", "Sample summary showing the number of reads and ASVs after PIME filtering.")
 caption_tab_its("its_pime_oob_best_prev", "Out of Bag error (OOB) from random forests for each prevalence interval. The number of taxa and the number of remaining reads for each prevalence interval are shown.")
 caption_tab_its("its_pime_filt_summary", "Comparison of the unfiltered data and PIME filtered data for the chosen prevalence interval.")
-caption_tab_its("its_asv_pime_sum", "Changes in read count and total ASVs during PIME filtering.")
+caption_tab_its("its_pime_change_by_year_treat", "Changes in read count and total ASVs during PIME filtering.")
 
 #<small>`r caption_tab_ssu("qual_scores_after_ssu")`</small>
 
---
+++ assets/reactable/download_this_fun.R
@@ -0,0 +1,13 @@
+download_this_fun <- function(input_tab) {
+  out_name <- deparse(substitute(input_tab))
+  seq_table <<- input_tab
+  seq_table %>%
+  download_this(
+    output_name = out_name,
+    output_extension = ".csv",
+    button_label = "Download data as csv file",
+    button_type = "default",
+    csv2 = FALSE,
+    has_icon = TRUE,
+    icon = "fa fa-save")
+  }
